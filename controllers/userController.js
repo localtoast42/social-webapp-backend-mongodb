364883b3657698ae3c3a22121bfe9e79aaeabc78
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-23 18:03:31 -0400

    Add userController create, update, delete funcs

---
+++ controllers/userController.js
@@ -0,0 +1,121 @@
+import User from '../models/user';
+import bcrypt from 'bcryptjs'
+import asyncHandler from 'express-async-handler';
+import { body, validationResult } from 'express-validator';
+
+function isUserCreator(req, res, next) {
+  User.findOne({ _id: req.params.userId })
+    .then((user) => {
+      if (user.id === req.user.id) {
+        return next();
+      } else {
+        return res.status(401).json({ success: false, msg: "Unauthorized" });
+      }
+    })
+    .catch((err) => next(err));
+};
+
+exports.user_create = [
+  body("username")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Username must be provided.")
+    .isAlphanumeric()
+    .withMessage("Username has non-alphanumeric characters."),
+  body("password")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Password must be provided."),
+  body("confirm_password")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Password must be provided.")
+    .custom((value, { req }) => {
+        return value === req.body.password;
+    })
+    .withMessage("Passwords must match."),
+  body("firstName")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("First name must be provided.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("lastName")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Last name must be provided.")
+    .isAlphanumeric()
+    .withMessage("Last name has non-alphanumeric characters."),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const user = new User({
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    if (!errors.isEmpty()) {
+      res.send(errors.array());
+    } else {
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hashedPassword;
+        const result = await user.save();
+        res.status(201).end();
+      });
+    };
+  }),
+];
+
+exports.user_update = [
+  isUserCreator,
+
+  body("firstName")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("First name must be provided.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("lastName")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Last name must be provided.")
+    .isAlphanumeric()
+    .withMessage("Last name has non-alphanumeric characters."),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    if (!errors.isEmpty()) {
+      res.send(errors.array());
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(req.params.userId, user, {});
+      res.status(200).end();
+    }
+  }),
+];
+
+exports.user_delete = [
+  isUserCreator,
+
+  asyncHandler(async (req, res, next) => {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).end();
+  }),
+];
