734cf441b0d681922ff7375550fed0b9c0544dee
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-23 14:40:13 -0400

    Add commentController functions

---
+++ controllers/commentController.js
@@ -0,0 +1,77 @@
+import Comment from '../models/comment';
+import asyncHandler from 'express-async-handler';
+import { body, validationResult } from 'express-validator';
+
+function isCommentAuthor(req, res, next) {
+  Comment.findOne({ _id: req.params.commentId })
+    .then((comment) => {
+      if (comment.author.toString() === req.user.id) {
+        return next();
+      } else {
+        return res.status(401).json({ success: false, msg: "Unauthorized" });
+      }
+    })
+    .catch((err) => next(err));
+};
+
+
+exports.comment_create = [
+  body("text")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Comment must not be empty."),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const comment = new Comment({
+      author: req.user.id,
+      text: req.body.text,
+      post: req.params.postId,
+      postDate: Date.now(),
+    });
+
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors);
+    } else {
+      const newComment = await comment.save();
+      res.status(201).json(newComment);
+    };
+  }),
+];
+
+exports.comment_update = [
+  isCommentAuthor,
+
+  body("text")
+    .trim()
+    .isLength( { min: 1 })
+    .escape()
+    .withMessage("Comment must not be empty."),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const comment = await Comment.findOne({ _id: req.params.commentId });
+
+    comment.text = req.body.text;
+    comment.lastEditDate = Date.now();
+
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors);
+    } else {
+      const updatedComment = await Comment.findByIdAndUpdate(comment._id, comment, {});
+      res.status(200).end();
+    }
+  }),
+];
+
+exports.comment_delete = [
+  isCommentAuthor,
+
+  asyncHandler(async (req, res, next) => {   
+    await Comment.findByIdAndDelete(req.params.commentId);
+    res.status(200).end();
+  }),
+];
