30fea4933a42e8dff91452a7fb7b52f4204e389a
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-23 13:55:08 -0400

    Add lastEditDate to post model schema

--- models/post.js
+++ models/post.js
@@ -7,6 +7,7 @@ const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   text: { type: String, minLength: 1, required: true },
   postDate: { type: Date, required: true },
+  lastEditDate: { type: Date },
   likes: [{ type: Schema.Types.ObjectId, ref: "User" }]
 });
 
@@ -16,6 +17,10 @@ PostSchema.virtual("url").get(function () {
 
 PostSchema.virtual("postDateFormatted").get(function () {
   return this.postDate ? DateTime.fromJSDate(this.postDate).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+PostSchema.virtual("lastEditDateFormatted").get(function () {
+  return this.lastEditDate ? DateTime.fromJSDate(this.lastEditDate).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 export default mongoose.model("Post", PostSchema);
