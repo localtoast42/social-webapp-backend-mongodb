f083967b87060f32e16e60be8d55a48ced3b9040
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-23 13:46:23 -0400

    Remove isPublished from post model schema

--- models/post.js
+++ models/post.js
@@ -7,8 +7,7 @@ const PostSchema = new Schema({
   title: { type: String, required: true, minLength: 3, maxLength: 100, default: "New Post" },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   text: { type: String },
-  isPublished: { type: Boolean, default: false },
-  publishedDate: { type: Date },
+  postDate: { type: Date },
   likes: [{ type: Schema.Types.ObjectId, ref: "User" }]
 });
 
@@ -16,8 +15,8 @@ PostSchema.virtual("url").get(function () {
   return `/posts/${this._id}`;
 });
 
-PostSchema.virtual("publishedDateFormatted").get(function () {
-  return this.publishedDate ? DateTime.fromJSDate(this.publishedDate).toLocaleString(DateTime.DATE_MED) : '';
+PostSchema.virtual("postDateFormatted").get(function () {
+  return this.postDate ? DateTime.fromJSDate(this.postDate).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 export default mongoose.model("Post", PostSchema);
