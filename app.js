42374f9ea7d7b52c3aa537b64aa22eff4d8eb618
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-30 15:15:12 -0400

    Remove unused views directory

--- app.js
+++ app.js
@@ -26,11 +26,6 @@ mongoose.connect(mongoDB);
 passportConfig(passport);
 app.use(passport.initialize());
 
-// view engine setup
-const viewsURL = new URL('views', import.meta.url);
-app.set('views', viewsURL.toString());
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -53,8 +48,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).end();
 });
 
 export default app;
