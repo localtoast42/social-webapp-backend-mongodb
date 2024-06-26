0c72a24aff4650f103372d9ef054a322ee0e843d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-26 16:58:28 -0400

    Add more properties to User schema

--- models/user.js
+++ models/user.js
@@ -5,8 +5,13 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true, minLength: 3, maxLength: 100 },
   password: { type: String, required: true },
+  imageUrl: { type: String },
   firstName: { type: String, required: true, maxLength: 100 },
   lastName: { type: String, required: true, maxLength: 100 },
+  city: { type: String, required: true, maxLength: 200 },
+  state: { type: String, required: true, maxLength: 200 },
+  country: { type: String, required: true, maxLength: 200 },
+  isAdmin: { type: Boolean, required: true, default: false },
   followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
   following: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
