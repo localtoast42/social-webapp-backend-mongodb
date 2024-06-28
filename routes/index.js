30252637e32f04c31e97be1fbec2516f55043b61
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-27 20:01:42 -0400

    Add endpoint for guest login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import usersRouter from './users.js';
 import postsRouter from './posts.js';
+import { guest_create } from '../controllers/userController.js';
 import { login } from './auth.js';
 
 const indexRouter = express.Router();
 
 indexRouter.post('/login', login);
+indexRouter.post('/login/guest', guest_create, login);
 
 indexRouter.use('/users', usersRouter)
 indexRouter.use('/posts', postsRouter)
