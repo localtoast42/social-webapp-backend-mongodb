87ff88ff704b8c5b4fdb11cad70cb4bf5d559ac3
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-29 14:05:52 -0400

    Refactor queries with promises, handle exceptions

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,114 +15,120 @@ function isPostAuthor(req, res, next) {
 };
 
 export const get_posts = asyncHandler(async (req, res, next) => {
-  const posts = await Post.find()
+  Post.find()
     .sort("-postDate")
     .populate("author")
     .limit(req.query.limit)
-    .exec();
+    .exec()
+    .then((posts) => {
+      const postsData = posts.map(post => {
+        return {
+          id: post.id,
+          url: post.url,
+          text: post.text,
+          dateTime: post.postDate,
+          date: post.postDateFormatted,
+          lastEditDate: post.lastEditDateFormatted,
+          author: {
+            id: post.author.id,
+            username: post.author.username,
+            fullName: post.author.fullName,
+            imageUrl: post.author.imageUrl,
+            url: post.author.url
+          },
+          numLikes: post.likes.length,
+          isLiked: post.likes.includes(req.user.id),
+          numComments: post.comments.length
+        }
+      })
 
-  const postsData = posts.map(post => {
-    return {
-      id: post.id,
-      url: post.url,
-      text: post.text,
-      dateTime: post.postDate,
-      date: post.postDateFormatted,
-      lastEditDate: post.lastEditDateFormatted,
-      author: {
-        id: post.author.id,
-        username: post.author.username,
-        fullName: post.author.fullName,
-        imageUrl: post.author.imageUrl,
-        url: post.author.url
-      },
-      numLikes: post.likes.length,
-      isLiked: post.likes.includes(req.user.id),
-      numComments: post.comments.length
-    }
-  })
-
-  res.status(200).json(postsData);
+      return res.status(200).json(postsData);
+    })
+    .catch((err) => next(err));
 });
 
 export const get_followed_posts = asyncHandler(async (req, res, next) => {
-  const followedPosts = await Post.find()
+  Post.find()
     .where("author").in([req.user.id, ...req.user.following])
     .sort("-postDate")
     .populate("author")
-    .exec();
+    .exec()
+    .then((followedPosts) => {
+      const followedPostsData = followedPosts.map(post => {
+        return {
+          id: post.id,
+          url: post.url,
+          text: post.text,
+          dateTime: post.postDate,
+          date: post.postDateFormatted,
+          lastEditDate: post.lastEditDateFormatted,
+          author: {
+            id: post.author.id,
+            username: post.author.username,
+            fullName: post.author.fullName,
+            imageUrl: post.author.imageUrl,
+            url: post.author.url
+          },
+          numLikes: post.likes.length,
+          isLiked: post.likes.includes(req.user.id),
+          numComments: post.comments.length
+        }
+      });
 
-  const followedPostsData = followedPosts.map(post => {
-    return {
-      id: post.id,
-      url: post.url,
-      text: post.text,
-      dateTime: post.postDate,
-      date: post.postDateFormatted,
-      lastEditDate: post.lastEditDateFormatted,
-      author: {
-        id: post.author.id,
-        username: post.author.username,
-        fullName: post.author.fullName,
-        imageUrl: post.author.imageUrl,
-        url: post.author.url
-      },
-      numLikes: post.likes.length,
-      isLiked: post.likes.includes(req.user.id),
-      numComments: post.comments.length
-    }
-  })
-
-  res.status(200).json(followedPostsData);
+      return res.status(200).json(followedPostsData);
+    })
+    .catch((err) => next(err));
 });
 
 export const post_get = asyncHandler(async (req, res, next) => {
-  const post = await Post.findById(req.params.postId)
+  Post.findById(req.params.postId)
     .populate("author")
     .populate({
       path: "comments",
       options: { sort: { postDate: 1 }},
       populate: { path: "author" }
     })
-    .exec();
+    .exec()
+    .then((post) => {
+      const comments = post.comments.map(comment => {
+        return {
+          id: comment.id,
+          post: comment.post,
+          text: comment.text,
+          date: comment.postDateFormatted,
+          author: {
+            id: comment.author.id,
+            username: comment.author.username,
+            fullName: comment.author.fullName,
+            imageUrl: comment.author.imageUrl,
+            url: comment.author.url
+          },
+          numLikes: comment.likes.length,
+          isLiked: comment.likes.includes(req.user.id),
+        }
+      });
 
-  const comments = post.comments.map(comment => {
-    return {
-      id: comment.id,
-      post: comment.post,
-      text: comment.text,
-      date: comment.postDateFormatted,
-      author: {
-        id: comment.author.id,
-        username: comment.author.username,
-        fullName: comment.author.fullName,
-        imageUrl: comment.author.imageUrl,
-        url: comment.author.url
-      },
-      numLikes: comment.likes.length,
-      isLiked: comment.likes.includes(req.user.id),
-    }
-  })
+      const postData = {
+        id: post.id,
+        text: post.text,
+        date: post.postDateFormatted,
+        lastEditDate: post.lastEditDateFormatted,
+        author: {
+          id: post.author.id,
+          username: post.author.username,
+          fullName: post.author.fullName,
+          imageUrl: post.author.imageUrl,
+          url: post.author.url
+        },
+        comments: comments,
+        numLikes: post.likes.length,
+        isLiked: post.likes.includes(req.user.id),
+        numComments: post.comments.length
+      };
 
-  const postData = {
-    id: post.id,
-    text: post.text,
-    date: post.postDateFormatted,
-    lastEditDate: post.lastEditDateFormatted,
-    author: {
-      id: post.author.id,
-      username: post.author.username,
-      fullName: post.author.fullName,
-      imageUrl: post.author.imageUrl,
-      url: post.author.url
-    },
-    comments: comments,
-    numLikes: post.likes.length,
-    isLiked: post.likes.includes(req.user.id),
-    numComments: post.comments.length
-  }
-
-  res.status(200).json(postData);
+      return res.status(200).json(postData);
+    })
+    .catch((err) => next(err));
 });
 
 export const post_create = [
@@ -135,17 +141,20 @@ export const post_create = [
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
 
-    const post = new Post({
-      author: req.user.id,
-      text: req.body.text,
-      postDate: Date.now()
-    });
-
     if (!errors.isEmpty()) {
       res.status(400).json(errors);
     } else {
-      const newPost = await post.save();
-      res.status(201).json(newPost);
+      const post = new Post({
+        author: req.user.id,
+        text: req.body.text,
+        postDate: Date.now()
+      });
+      
+      post.save()
+        .then((newPost) => {
+          return res.status(201).json(newPost);
+        })
+        .catch((err) => next(err));
     };
   }),
 ];
@@ -162,16 +171,19 @@ export const post_update = [
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
 
-    const post = await Post.findOne({ _id: req.params.postId });
-
-    post.text = req.body.text;
-    post.lastEditDate = Date.now();
-
     if (!errors.isEmpty()) {
       res.status(400).json(errors);
     } else {
-      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {});
-      res.status(200).json(updatedPost);
+      Post.findOne({ _id: req.params.postId })
+        .then((post) => {
+          post.text = req.body.text;
+          post.lastEditDate = Date.now();
+          return Post.findByIdAndUpdate(post._id, post, {});
+        })
+        .then((updatedPost) => {
+          return res.status(200).json(updatedPost);
+        })
+        .catch((err) => next(err));
     };
   }),
 ];
@@ -180,21 +192,30 @@ export const post_delete = [
   isPostAuthor,
 
   asyncHandler(async (req, res, next) => {    
-      await Comment.deleteMany({ post: req.params.postId });
-      await Post.findByIdAndDelete(req.params.postId);
-      res.status(200).end();
+    Comment.deleteMany({ post: req.params.postId })
+      .then((response) => {
+        return Post.findByIdAndDelete(req.params.postId);
+      })
+      .then((response) => {
+        return res.status(200).end();
+      })
+      .catch((err) => next(err));
   }),
 ]
 
 export const post_modify_likes = asyncHandler(async (req, res, next) => {
-  const post = await Post.findOne({ _id: req.params.postId });
+  Post.findOne({ _id: req.params.postId })
+    .then((post) => {
+      if (req.body.like && !post.likes.includes(req.user.id)) {
+        post.likes.push(req.user.id);
+      } else {
+        post.likes = post.likes.filter((userid) => userid != req.user.id);
+      }
 
-  if (req.body.like && !post.likes.includes(req.user.id)) {
-    post.likes.push(req.user.id);
-  } else {
-    post.likes = post.likes.filter((userid) => userid != req.user.id);
-  }
-
-  const updatedPost = await post.save();
-  res.status(200).json(updatedPost);
+      return post.save();
+    })
+    .then((updatedPost) => {
+      return res.status(200).json(updatedPost);
+    })
+    .catch((err) => next(err));
 });
