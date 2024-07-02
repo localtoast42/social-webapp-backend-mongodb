71eea5ed813c2be99552b1892618c703ef754b08
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-02 15:45:50 -0400

    Add more descriptive error msgs to login route

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,25 @@ export const login = asyncHandler(async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (!user) {
-      return res.status(401).json({ success: false, msg: "Incorrect username" });
+      return res.status(401).json(
+        { 
+          success: false, 
+          username: {
+            msg: "User not found, try different username" 
+          }
+        }
+      );
     };
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) {
-      return res.status(401).json({ success: false, msg: "Incorrect password" });
+      return res.status(401).json(
+        { 
+          success: false, 
+          password: {
+            msg: "Incorrect password" 
+          }
+        }
+      );
     }
     const tokenObject = issueJWT(user);
     res
