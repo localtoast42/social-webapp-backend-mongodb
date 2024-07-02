90f657468bd96fda5b01622989f81ee608d04a0f
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-02 16:43:17 -0400

    Filter comments shown by isPublicComment

--- controllers/postController.js
+++ controllers/postController.js
@@ -87,6 +87,7 @@ export const post_get = asyncHandler(async (req, res, next) => {
     .populate("author")
     .populate({
       path: "comments",
+      match: { $or: [{ author: req.user.id }, { isPublicComment: true }] },
       options: { sort: { postDate: 1 }},
       populate: { path: "author" }
     })
