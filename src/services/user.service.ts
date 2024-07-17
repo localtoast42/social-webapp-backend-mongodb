8cce80e4801dd9616dc981ff830d8c47e1a3be77
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 12:33:20 -0400

    Add validatePassword function

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,5 @@
 import { FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
+import { omit } from "lodash";
 import UserModel, { User, UserInput } from "../models/user.model.js";
 
 export async function createUser(input: UserInput) {
@@ -7,6 +8,25 @@ export async function createUser(input: UserInput) {
   } catch (e: any) {
     throw new Error(e);
   }
+}
+
+export async function validatePassword({
+  username, password
+}: {
+  username: string, 
+  password: string
+}) {
+  const user = await UserModel.findOne({username});
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), "password");
 }
 
 export async function findUser(query: FilterQuery<User>) {
