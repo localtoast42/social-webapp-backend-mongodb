74040c050d0a8035c637c7653a46e2c057ea506a
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 14:30:54 -0400

    Issue new access token if refresh token is valid

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
-import { get } from 'lodash';
 import { verifyJwt } from '../utils/jwt.utils.js';
+import { reIssueAccessToken } from '../services/session.service.js';
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
-  const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
+const deserializeUser = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+  const accessToken = (req.get('authorization') ?? "").replace(/^Bearer\s/, "");
+  const refreshToken = req.get('x-refresh') ?? "";
 
   if (!accessToken) {
     return next();
@@ -13,6 +18,19 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
 
   if (decoded) {
     res.locals.user = decoded;
+    return next();
+  }
+
+  if (expired && refreshToken) {
+    const newAccessToken = await reIssueAccessToken({ refreshToken });
+
+    if (newAccessToken) {
+      res.setHeader('x-access-token', newAccessToken)
+    }
+
+    const result = verifyJwt(newAccessToken as string);
+
+    res.locals.user = result.decoded;
     return next();
   }
 
