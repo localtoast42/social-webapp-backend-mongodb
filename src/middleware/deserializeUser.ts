efe1207347668d8b4faf32d098bfab33023d443a
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 13:35:20 -0400

    Add next function call in default case

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -15,6 +15,8 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
     res.locals.user = decoded;
     return next();
   }
+
+  return next();
 };
 
 export default deserializeUser;
