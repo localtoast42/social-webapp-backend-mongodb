a8938b62ba7e2ef6c55bb047ba454886a632d535
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 12:57:10 -0400

    Add session model definition

---
+++ src/models/session.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { User } from "./user.model.js";
+
+export interface Session {
+  user: User["id"];
+  valid: boolean;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const SessionModel = mongoose.model<Session>("Session", sessionSchema);
+
+export default SessionModel;
