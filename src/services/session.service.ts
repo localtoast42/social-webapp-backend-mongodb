0aeb0292c4a5ebdc7ae571677c3f9500bc1513d8
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 13:20:00 -0400

    Add findSessions function to session service

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,7 +1,12 @@
-import SessionModel, { SessionInput } from "../models/session.model.js";
+import { FilterQuery } from "mongoose";
+import SessionModel, { Session, SessionInput } from "../models/session.model.js";
 
 export async function createSession(input: SessionInput) {
   const session = await SessionModel.create(input);
 
   return session.toJSON();
+}
+
+export async function findSessions(query: FilterQuery<Session>) {
+  return SessionModel.find(query).lean();
 }
