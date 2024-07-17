20462d1781f07f4d7e9636e328f51cbffcba6b8f
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 16:12:50 -0400

    Add new input fields to user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,14 +3,23 @@ import { object, string, TypeOf } from 'zod';
 export const createUserSchema = object({
   body: object({
     username: string({
-      required_error: 'Username is required'
+      required_error: 'Username must be provided'
     }),
     password: string({
-      required_error: 'Password is required'
+      required_error: 'Password must be provided'
     }).min(6, "Password must be at least 6 characters long"),
     passwordConfirmation: string({
-      required_error: 'Password confirmation is required'
+      required_error: 'Password confirmation must be provided'
     }),
+    firstName: string({
+      required_error: 'First name must be provided'
+    }),
+    lastName: string({
+      required_error: 'Last name must be provided'
+    }),
+    city: string(),
+    state: string(),
+    country: string(),
   }).refine((data) => data.password === data.passwordConfirmation, {
     message: "Passwords do not match",
     path: ["passwordConfirmation"],
