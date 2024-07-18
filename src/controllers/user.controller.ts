f0fdcd590967dd299db833095b907d1d705edb19
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-17 20:54:47 -0400

    Refactor user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,33 +1,65 @@
-import User, { IUser } from '../models/user.model.js';
-import Post from '../models/post.model.js';
-import bcrypt from 'bcryptjs';
-import asyncHandler from 'express-async-handler';
-import { faker } from '@faker-js/faker';
-import { body, validationResult } from 'express-validator';
-import { Request, Response, NextFunction } from 'express';
-import { createRandomUser, createRandomPost } from '../scripts/populatedb.js';
+import { Request, Response } from 'express';
+import { omit } from 'lodash';
+import logger from '../utils/logger.js';
+import { 
+  createUser, 
+  findUser, 
+  findUsersByName, 
+  findAndUpdateUser, 
+  deleteUser 
+} from '../services/user.service.js';
+import { CreateUserInput } from '../schemas/user.schema.js';
+import { createRandomPost, createRandomUser } from '../utils/populateDatabase.js';
 
-function isAdmin(req: Request, res: Response, next: NextFunction) {
-  if (req.user.isAdmin) {
-    next();
-  } else {
-    res.status(401).json({ success: false, msg: "Unauthorized" });
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>, 
+  res: Response
+) {
+  try {
+    const user = await createUser(req.body);
+    return res.send(user);
+  } catch (e: any) {
+    logger.error(e);
+    return res.status(409).send(e.message);
   }
-};
+}
 
-function isUserCreator(req: Request, res: Response, next: NextFunction) {
-  User.findOne({ _id: req.params.userId })
-    .then((user) => {
-      if (user.id === req.user.id) {
-        next();
-      } else {
-        res.status(401).json({ success: false, msg: "Unauthorized" });
-      }
-    })
-    .catch((err) => next(err));
-};
+export async function getUserHandler(
+  req: Request, 
+  res: Response
+) {
+  const userId = req.params.userId;
+  const user = await findUser({ userId });
 
-export const user_list_get = asyncHandler(async (req, res, next) => {
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  user.followedByMe = res.locals.user.following.includes(user._id);
+
+  return res.send(omit(user, "password"));
+}
+
+export async function getSelfHandler(
+  req: Request, 
+  res: Response
+) {
+  const userId = res.locals.user._id;
+  const user = await findUser({ userId });
+
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  return res.send(omit(user, "password"));
+}
+
+export async function getUserListHandler(
+  req: Request, 
+  res: Response
+) {
+  const userId = res.locals.user._id;
+
   const queryTerms: object[] = [];
   queryTerms.push({ _id: { $exists: true} });
 
@@ -37,327 +69,170 @@ export const user_list_get = asyncHandler(async (req, res, next) => {
     queryTerms.push({ $or: [{ firstName: regex }, { lastName: regex }]});
   } 
 
-  User.find()
-  .where("_id").ne(req.user.id)
-  .where("isGuest").ne(true)
-  .and(queryTerms)
-  .sort("lastName")
-  .exec()
-  .then((allUsers) => {
-    const allUsersData = allUsers.map(user => {
-      return {
-        id: user.id,
-        username: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        fullName: user.fullName,
-        imageUrl: user.imageUrl,
-        url: user.url,
-      }
-    })
-    res.status(200).json(allUsersData);
-  })
-  .catch((err) => next(err));
-});
-
-export const user_get = asyncHandler(async (req, res, next) => {
-  User.findOne({ _id: req.params.userId })
-    .then((user) => {
-      const userData = {
-        id: user.id,
-        username: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        fullName: user.fullName,
-        imageUrl: user.imageUrl,
-        url: user.url,
-        followedByMe: req.user.following.includes(user.id),
-      }
-
-      res.status(200).json(userData);
-    })
-    .catch((err) => next(err));
-});
-
-export const user_self_get = asyncHandler(async (req, res, next) => {
-  const user = {
-    id: req.user.id,
-    firstName: req.user.firstName,
-    lastName: req.user.lastName,
-    city: req.user.city,
-    state: req.user.state,
-    country: req.user.country,
-    fullName: req.user.fullName,
-    username: req.user.username,
-    imageUrl: req.user.imageUrl,
-    isAdmin: req.user.isAdmin,
-    url: req.user.url,
-    hasFollows: req.user.following.length !== 0,
+  const query = { 
+    _id: { $ne: userId }, 
+    isGuest: false,
+    $and: queryTerms,
   }
 
-  res.status(200).json(user);
-});
+  const options = {
+    sort: { "lastName": 1 },
+  }
 
-export const self_following_get = asyncHandler(async (req, res, next) => {
-  res.status(200).json(req.user.following);
-});
+  const users = await findUsersByName(query, options);
 
-export const user_create = [
-  body("username")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("Username must be provided.")
-    .isAlphanumeric()
-    .withMessage("Username has non-alphanumeric characters."),
-  body("password")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("Password must be provided."),
-  body("confirm_password")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("Password must be provided.")
-    .custom((value, { req }) => {
-        return value === req.body.password;
-    })
-    .withMessage("Passwords must match."),
-  body("firstName")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("First name must be provided.")
-    .isAlphanumeric()
-    .withMessage("First name has non-alphanumeric characters."),
-  body("lastName")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("Last name must be provided.")
-    .isAlphanumeric()
-    .withMessage("Last name has non-alphanumeric characters."),
+  return res.send(users);
+}
 
-  asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
+export async function updateUserHandler(
+  req: Request, 
+  res: Response
+) {
+  const requestingUserId = res.locals.user._id;
+  const userId = req.params.userId;
+  const update = req.body;
 
-    if (!errors.isEmpty()) {
-      res.send(errors.mapped());
-    } else {
-      const user = new User({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        isGuest: process.env.ALLOW_NEW_PUBLIC_USERS === "false",
-      });
+  const user = await findUser({ userId });
 
-      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        if (err) {
-          next(err);
-        }
-        user.password = hashedPassword;
-        
-        user.save()
-          .then((result) => {
-            next();
-          })
-          .catch((err) => next(err));
-      });
-    };
-  }),
-];
+  if (!user) {
+    return res.sendStatus(404);
+  }
 
-export const guest_create = asyncHandler(async (req, res, next) => {
-  const guestNum = faker.string.alphanumeric(8);
+  if (user._id !== requestingUserId) {
+    return res.sendStatus(403);
+  }
 
-  const user = new User({
-    username: `Guest_#${guestNum}`,
-    firstName: "Guest",
-    lastName: `#${guestNum}`,
-    isGuest: true,
+  const updatedUser = await findAndUpdateUser({ userId }, update, { 
+    new: true, 
   });
 
-  bcrypt.hash("guest", 10, async (err, hashedPassword) => {
-    if (err) {
-      next(err);
+  return res.send(updatedUser);
+}
+
+export async function deleteUserHandler(
+  req: Request, 
+  res: Response
+) {
+  const requestingUserId = res.locals.user._id;
+  const userId = req.params.userId;
+
+  const user = await findUser({ userId });
+
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  if (user._id !== requestingUserId) {
+    return res.sendStatus(403);
+  }
+
+  await deleteUser({ userId });
+
+  return res.sendStatus(200);
+}
+
+export async function getUserFollowsHandler(
+  req: Request, 
+  res: Response
+) {
+  const userId = req.params.userId;
+  const userFollows = await findUser({ userId }, "following" );
+
+  return res.send(userFollows);
+}
+
+export async function followUserHandler(
+  req: Request, 
+  res: Response
+) {
+  const requestingUserId = res.locals.user._id;
+  const targetUserId = req.params.userId;
+
+  const [requestingUser, targetUser] = await Promise.all([
+    findUser({ requestingUserId }),
+    findUser({ targetUserId })
+  ])
+
+  if (!targetUser || !requestingUser) {
+    return res.sendStatus(404);
+  }
+
+  const requestingUserUpdates = requestingUser.following;
+  const targetUserUpdates = targetUser.followers;
+
+  if (!requestingUser.following.includes(targetUser._id)) {
+    requestingUserUpdates.push(targetUser._id);
+  }
+
+  if (!targetUser.followers.includes(requestingUser._id)) {
+    targetUserUpdates.push(requestingUser._id);
+  }
+
+  await Promise.all([
+    findAndUpdateUser(
+      { requestingUserId }, 
+      requestingUserUpdates, 
+      { new: true }
+    ),
+    findAndUpdateUser(
+      { targetUserId }, 
+      targetUserUpdates, 
+      { new: true }
+    )
+  ])
+
+  return res.sendStatus(200);
+}
+
+export async function unfollowUserHandler(
+  req: Request, 
+  res: Response
+) {
+  const requestingUserId = res.locals.user._id;
+  const targetUserId = req.params.userId;
+
+  const [requestingUser, targetUser] = await Promise.all([
+    findUser({ requestingUserId }),
+    findUser({ targetUserId })
+  ])
+
+  if (!targetUser || !requestingUser) {
+    return res.sendStatus(404);
+  }
+
+  const requestingUserUpdates = requestingUser.following.filter((userId) => userId !== targetUser._id);
+  const targetUserUpdates = targetUser.followers.filter((userId) => userId !== requestingUser._id)
+
+  await Promise.all([
+    findAndUpdateUser(
+      { requestingUserId }, 
+      requestingUserUpdates, 
+      { new: true }
+    ),
+    findAndUpdateUser(
+      { targetUserId }, 
+      targetUserUpdates, 
+      { new: true }
+    )
+  ])
+
+  return res.sendStatus(200);
+}
+
+export async function populateUsers(
+  req: Request, 
+  res: Response
+) {
+  const userCount = req.body.userCount;
+  const postCount = req.body.postCount;
+
+  for (let i = 0; i < userCount; i++) {
+    let user = await createRandomUser();
+
+    for (let j = 0; j < postCount; j++) {
+      createRandomPost(user._id);
     }
-    user.password = hashedPassword;
+  }
 
-    user.save()
-      .then((result) => {
-        req.body.username = result.username;
-        req.body.password = "guest";
-        next();
-      })
-      .catch((err) => next(err));
-  });
-});
-
-export const populate_users = [
-  isAdmin,
-  
-  asyncHandler(async (req, res, next) => {
-    const userCount = req.body.userCount;
-    const postCount = req.body.postCount;
-
-    for (let i = 0; i < userCount; i++) {
-      let user = createRandomUser();
-
-      bcrypt.hash(user.password, 10, async (err, hashedPassword) => {
-        if (err) {
-          next(err);
-        }
-        user.password = hashedPassword;
-
-        let newUser = await user.save();
-
-        for (let j = 0; j < postCount; j++) {
-          let post = createRandomPost(newUser);
-          let newPost = await post.save();
-        }
-      });
-    }
-
-    res.status(201).end();
-  })
-];
-
-export const user_update = [
-  isUserCreator,
-
-  body("firstName")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("First name must be provided.")
-    .isAlphanumeric()
-    .withMessage("First name has non-alphanumeric characters."),
-  body("lastName")
-    .trim()
-    .isLength( { min: 1 })
-    .escape()
-    .withMessage("Last name must be provided.")
-    .isAlphanumeric()
-    .withMessage("Last name has non-alphanumeric characters."),
-
-  asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.send(errors.mapped());
-    } else {
-      User.findOne({ _id: req.params.userId })
-        .then((user) => {
-          user.username = req.body.username;
-          user.firstName = req.body.firstName;
-          user.lastName = req.body.lastName;
-          user.city = req.body.city;
-          user.state = req.body.state;
-          user.country = req.body.country;
-
-          User.findByIdAndUpdate(user._id, user, {})
-        })
-        .then((updatedUser) => {
-          res.status(200).end();
-        })
-        .catch((err) => next(err));
-    }
-  }),
-];
-
-export const user_delete = [
-  isUserCreator,
-
-  asyncHandler(async (req, res, next) => {
-    User.findByIdAndDelete(req.params.userId)
-      .then((result) => {
-        res.status(200).end();
-      })
-      .catch((err) => next(err));
-  }),
-];
-
-export const get_posts_by_user = asyncHandler(async (req, res, next) => {
-  Post.find({ author: req.params.userId })
-    .populate<{ author: IUser }>("author")
-    .sort("-postDate")
-    .exec()
-    .then((postsByUser) => {
-      const postsByUserData = postsByUser.map(post => {
-        return {
-          id: post.id,
-          url: post.url,
-          text: post.text,
-          dateTime: post.postDate,
-          date: post.postDateFormatted,
-          lastEditDate: post.lastEditDateFormatted,
-          author: {
-            id: post.author.id,
-            username: post.author.username,
-            fullName: post.author.fullName,
-            imageUrl: post.author.imageUrl,
-            url: post.author.url
-          },
-          numLikes: post.likes.length,
-          isLiked: post.likes.includes(req.user.id),
-          numComments: post.comments.length
-        }
-      });
-      res.status(200).json(postsByUserData);
-    })
-    .catch((err) => next(err));
-});
-
-export const user_following_get = asyncHandler(async (req, res, next) => {
-  User.findOne({ _id: req.params.userId }, "following")
-    .then((following) => {
-      res.status(200).json(following);
-    })
-    .catch((err) => next(err));
-});
-
-export const user_follow = asyncHandler(async (req, res, next) => {
-  const user = req.user;
-  
-  User.findOne({ _id: req.params.userId })
-    .then((target) => {
-      const promises = [];
-
-      if (!user.following.includes(target._id)) {
-        user.following.push(target._id);
-        promises.push(User.findByIdAndUpdate(user.id, user, {}));
-      }
-    
-      if (!target.followers.includes(user.id)) {
-        target.followers.push(user.id);
-        promises.push(User.findByIdAndUpdate(req.params.targetId, target, {}));
-      }
-
-      Promise.all(promises);
-    })
-    .then((responses) => {
-      res.status(200).end();
-    })
-    .catch((err) => next(err));
-});
-
-export const user_unfollow = asyncHandler(async (req, res, next) => {
-    const user = req.user;
-    
-    User.findOne({ _id: req.params.userId })
-      .then((target) => {
-        user.following = user.following.filter((userid) => userid.toString() !== target._id.toString());
-        target.followers = target.followers.filter((userid) => userid.toString() != user.id.toString());
-
-        const promises = [];
-        promises.push(User.findByIdAndUpdate(user.id, user, {}));
-        promises.push(User.findByIdAndUpdate(req.params.targetId, target, {}));
-
-        Promise.all(promises);
-      })
-      .then((responses) => {
-        res.status(200).end();
-      })
-      .catch((err) => next(err));
-});
+  res.sendStatus(201);
+}
