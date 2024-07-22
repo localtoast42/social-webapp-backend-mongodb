1a9556835e0c122019506d3fec3f27562e582e60
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-22 10:36:37 -0400

    Add config files

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,7 @@
+export default {
+    port: "PORT",
+    dbUri: "MONGODB_URI",
+    frontendUrl: "FRONTEND_URL",
+    accessTokenSecret: "ACCESS_TOKEN_SECRET",
+    refreshTokenSecret: "REFRESH_TOKEN_SECRET",
+}
---
+++ config/default.ts
@@ -0,0 +1,10 @@
+export default {
+    port: 3000,
+    dbUri: "",
+    frontendUrl: "",
+    saltWorkFactor: 10,
+    accessTokenTtl: "15m",
+    refreshTokenTtl: "1d",
+    accessTokenSecret: "",
+    refreshTokenSecret: "",
+}
