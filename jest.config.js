953fa5481e618592bdbae08e2133364166408c10
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-24 16:11:02 -0400

    Add transform back into jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
   testMatch: ["**/**/*.test.ts"],
   verbose: true,
   forceExit: true,
