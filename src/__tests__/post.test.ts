5acde0bc5765222ca80209d87f7050869287b2f7
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-26 17:17:47 -0500

    Add test case to create post route

--- src/__tests__/post.test.ts
+++ src/__tests__/post.test.ts
@@ -196,7 +196,31 @@ describe('post', () => {
       });
     });
 
-    describe('given the user is logged in', () => {
+    describe('given the user is logged in and sends empty text', () => {
+      it('should return a 400 with error message', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const createPostServiceMock = jest
+          .spyOn(PostService, 'createPost')
+          // @ts-ignore
+          .mockReturnValueOnce(postDocument.toJSON());
+
+        const { statusCode, body } = await supertest(app)
+          .post('/api/v1/posts')
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ text: "" });
+        
+        expect(statusCode).toBe(400);
+        expect(body[0].message).toEqual("Post must not be empty");
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(createPostServiceMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('given the user is logged in and sends text', () => {
       it('should return a 201 and create the post', async () => {
         const findUserServiceMock = jest
           .spyOn(UserService, 'findUser')
