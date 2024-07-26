7fbfe73cf8cfd4359710fb3328bf0c479286b254
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-26 17:56:32 -0500

    Add test cases for post liking

--- src/__tests__/post.test.ts
+++ src/__tests__/post.test.ts
@@ -397,7 +397,258 @@ describe('post', () => {
   });
 
   describe('like post route', () => {
+    describe('given the user is not logged in', () => {
+      it('should return a 401', async () => {   
+        const { statusCode } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .send({ like: "true" });
+          
+        expect(statusCode).toBe(401);
+      });
+    });
 
+    describe('given the postId is not a valid ObjectId', () => {
+      it('should return a 400 with error message', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          // @ts-ignore
+          .mockReturnValueOnce(postPayload);
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          // @ts-ignore
+          .mockReturnValueOnce(postDocument.toJSON());
+
+        const { statusCode, body } = await supertest(app)
+          .post(`/api/v1/posts/not_valid_id/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "true" });
+        
+        expect(statusCode).toBe(400);
+        expect(body[0].message).toEqual("Invalid postId");
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).not.toHaveBeenCalled();
+        expect(updatePostServiceMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('given like input is invalid', () => {
+      it('should return a 400 with error message', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          // @ts-ignore
+          .mockReturnValueOnce(postPayload);
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          // @ts-ignore
+          .mockReturnValueOnce(postDocument.toJSON());
+
+        const { statusCode, body } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "yes" });
+        
+        expect(statusCode).toBe(400);
+        expect(body[0].message).toEqual("Like must be true or false");
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).not.toHaveBeenCalled();
+        expect(updatePostServiceMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('given the post does not exist', () => {
+      it('should return a 404', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          // @ts-ignore
+          .mockReturnValueOnce(null);
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          // @ts-ignore
+          .mockReturnValueOnce(postDocument.toJSON());
+
+        const { statusCode } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "true" });
+        
+        expect(statusCode).toBe(404);
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).toHaveBeenCalledWith({ _id: postId });
+        expect(updatePostServiceMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('given like=true and the user has not previously liked the post', () => {
+      it('should add the user to likes and return the updated post', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          // @ts-ignore
+          .mockReturnValueOnce(postPayload);
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          .mockReturnValueOnce({
+            ...postDocument.toJSON(),
+            // @ts-ignore
+            likes: [ userId ],
+          });
+
+        const { statusCode, body } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "true" });
+        
+        expect(statusCode).toBe(200);
+        expect(body).toEqual({ 
+          ...postResponse,
+          likes: [ userId ],
+        });
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).toHaveBeenCalledWith({ _id: postId });
+        expect(updatePostServiceMock).toHaveBeenCalledWith(
+          { _id: postId },
+          { likes: [ userId ] },
+          { new: true }
+        );
+      });
+    });
+
+    describe('given like=true and the user has previously liked the post', () => {
+      it('should return the post with likes unmodified', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          .mockReturnValueOnce({
+            ...postPayload,
+            // @ts-ignore
+            likes: [ userId ],
+          });
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          .mockReturnValueOnce({
+            ...postDocument.toJSON(),
+            // @ts-ignore
+            likes: [ userId ],
+          });
+
+        const { statusCode, body } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "true" });
+        
+        expect(statusCode).toBe(200);
+        expect(body).toEqual({ 
+          ...postResponse,
+          likes: [ userId ],
+        });
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).toHaveBeenCalledWith({ _id: postId });
+        expect(updatePostServiceMock).toHaveBeenCalledWith(
+          { _id: postId },
+          { likes: [ userId ] },
+          { new: true }
+        );
+      });
+    });
+
+    describe('given like=false and the user has not previously liked the post', () => {
+      it('should return the post with likes unmodified', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          // @ts-ignore
+          .mockReturnValueOnce(postPayload);
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          // @ts-ignore
+          .mockReturnValueOnce(postDocument.toJSON());
+
+        const { statusCode, body } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "false" });
+        
+        expect(statusCode).toBe(200);
+        expect(body).toEqual(postResponse);
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).toHaveBeenCalledWith({ _id: postId });
+        expect(updatePostServiceMock).toHaveBeenCalledWith(
+          { _id: postId },
+          { likes: [] },
+          { new: true }
+        );
+      });
+    });
+
+    describe('given like=false and the user has previously liked the post', () => {
+      it('should remove the user from likes and return the updated post', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+        
+        const findPostServiceMock = jest
+          .spyOn(PostService, 'findPost')
+          .mockReturnValueOnce({
+            ...postPayload,
+            // @ts-ignore
+            likes: [ userId ],
+          });
+
+        const updatePostServiceMock = jest
+          .spyOn(PostService, 'findAndUpdatePost')
+          // @ts-ignore
+          .mockReturnValueOnce(postDocument.toJSON());
+
+        const { statusCode, body } = await supertest(app)
+          .post(`/api/v1/posts/${postId}/like`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send({ like: "false" });
+        
+        expect(statusCode).toBe(200);
+        expect(body).toEqual(postResponse);
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findPostServiceMock).toHaveBeenCalledWith({ _id: postId });
+        expect(updatePostServiceMock).toHaveBeenCalledWith(
+          { _id: postId },
+          { likes: [] },
+          { new: true }
+        );
+      });
+    });
   });
 
   describe('delete post route', () => {
