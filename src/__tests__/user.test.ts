5fee299b4065074f6c371e93eea3591d19fc215d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-30 13:52:29 -0400

    Add tests for user update route

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -15,6 +15,9 @@ const allowNewPublicUsers = config.get<boolean>('allowNewPublicUsers');
 const userObjectId = new mongoose.Types.ObjectId();
 const userId = userObjectId.toString();
 
+const otherUserObjectId = new mongoose.Types.ObjectId();
+const otherUserId = otherUserObjectId.toString();
+
 const userInput = {
   username: "testuser",
   firstName: "first",
@@ -49,6 +52,18 @@ const userResponse = {
   ...userDocument.toJSON(),
   "_id": userId,
   "followedByMe": false,
+};
+
+const updateUserInput = {
+  username: "testuser",
+  firstName: "first_new",
+  lastName: "last_new",
+  password: "testpwd",
+  passwordConfirmation: "testpwd",
+  city: "newcity",
+  state: "newstate",
+  country: "newcountry",
+  imageUrl: "newurl",
 };
 
 const sessionPayload = {
@@ -327,7 +342,202 @@ describe('user', () => {
   });
 
   describe('update user route', () => {
+    describe('given the user is not logged in', () => {
+      it('should return a 401', async () => {   
+        const { statusCode } = await supertest(app)
+          .put(`/api/v1/users/${userId}`)
+          .send(updateUserInput);
+          
+        expect(statusCode).toBe(401);
+      });
+    });
 
+    describe('given the request is bad', () => {
+      describe('because the userId is not a valid ObjectId', () => {
+        it('should return a 400 with error message', async () => {
+          const findUserServiceMock = jest
+            .spyOn(UserService, 'findUser')
+            // @ts-ignore
+            .mockReturnValueOnce(userPayload)
+            // @ts-ignore
+            .mockReturnValueOnce(userPayload);
+  
+          const updateUserServiceMock = jest
+            .spyOn(UserService, 'findAndUpdateUser')
+            // @ts-ignore
+            .mockReturnValueOnce({
+              ...userDocument.toJSON(),
+              ...updateUserInput
+            });
+    
+          const { statusCode, body } = await supertest(app)
+            .put(`/api/v1/users/not_valid_id`)
+            .set('Authorization', `Bearer ${jwt}`)
+            .send(updateUserInput);
+          
+          expect(statusCode).toBe(400);
+          expect(body[0].message).toEqual("Invalid userId");
+          expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+          expect(findUserServiceMock).toHaveBeenCalledTimes(1);
+          expect(updateUserServiceMock).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('because the firstName field is empty', () => {
+        it('should return a 400 with error message', async () => {
+          const findUserServiceMock = jest
+            .spyOn(UserService, 'findUser')
+            // @ts-ignore
+            .mockReturnValueOnce(userPayload)
+            // @ts-ignore
+            .mockReturnValueOnce(userPayload);
+  
+          const updateUserServiceMock = jest
+            .spyOn(UserService, 'findAndUpdateUser')
+            // @ts-ignore
+            .mockReturnValueOnce({
+              ...userDocument.toJSON(),
+              ...updateUserInput
+            });
+    
+          const { statusCode, body } = await supertest(app)
+            .put(`/api/v1/users/${userId}`)
+            .set('Authorization', `Bearer ${jwt}`)
+            .send({
+              ...updateUserInput,
+              firstName: "",
+            });
+          
+          expect(statusCode).toBe(400);
+          expect(body[0].message).toEqual("First name must not be empty");
+          expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+          expect(findUserServiceMock).toHaveBeenCalledTimes(1);
+          expect(updateUserServiceMock).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('because the lastName field is empty', () => {
+        it('should return a 400 with error message', async () => {
+          const findUserServiceMock = jest
+            .spyOn(UserService, 'findUser')
+            // @ts-ignore
+            .mockReturnValueOnce(userPayload)
+            // @ts-ignore
+            .mockReturnValueOnce(userPayload);
+  
+          const updateUserServiceMock = jest
+            .spyOn(UserService, 'findAndUpdateUser')
+            // @ts-ignore
+            .mockReturnValueOnce({
+              ...userDocument.toJSON(),
+              ...updateUserInput
+            });
+    
+          const { statusCode, body } = await supertest(app)
+            .put(`/api/v1/users/${userId}`)
+            .set('Authorization', `Bearer ${jwt}`)
+            .send({
+              ...updateUserInput,
+              lastName: "",
+            });
+          
+          expect(statusCode).toBe(400);
+          expect(body[0].message).toEqual("Last name must not be empty");
+          expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+          expect(findUserServiceMock).toHaveBeenCalledTimes(1);
+          expect(updateUserServiceMock).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('given the user does not exist', () => {
+      it('should return a 404', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload)
+          // @ts-ignore
+          .mockReturnValueOnce(null);
+
+        const updateUserServiceMock = jest
+          .spyOn(UserService, 'findAndUpdateUser')
+          // @ts-ignore
+          .mockReturnValueOnce({
+            ...userDocument.toJSON(),
+            ...updateUserInput
+          });
+
+        const { statusCode } = await supertest(app)
+          .put(`/api/v1/users/${userId}`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send(updateUserInput);
+        
+        expect(statusCode).toBe(404);
+        expect(findUserServiceMock).toHaveBeenCalledWith({ _id: userId });
+        expect(findUserServiceMock).toHaveBeenCalledTimes(2);
+        expect(updateUserServiceMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('given the requesting user is not the target user', () => {
+      it('should return a 403', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload)
+          // @ts-ignore
+          .mockReturnValueOnce({ ...userPayload, id: otherUserId });
+
+        const updateUserServiceMock = jest
+          .spyOn(UserService, 'findAndUpdateUser')
+          // @ts-ignore
+          .mockReturnValueOnce({
+            ...userDocument.toJSON(),
+            ...updateUserInput
+          });
+
+        const { statusCode } = await supertest(app)
+          .put(`/api/v1/users/${otherUserId}`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send(updateUserInput);
+        
+        expect(statusCode).toBe(403);
+        expect(findUserServiceMock).toHaveBeenCalledTimes(2);
+        expect(updateUserServiceMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('given the request is good', () => {
+      it('should return a 200 and the updated user', async () => {
+        const findUserServiceMock = jest
+          .spyOn(UserService, 'findUser')
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload)
+          // @ts-ignore
+          .mockReturnValueOnce(userDocument.toJSON());
+
+        const updateUserServiceMock = jest
+          .spyOn(UserService, 'findAndUpdateUser')
+          // @ts-ignore
+          .mockReturnValueOnce({
+            ...userDocument.toJSON(),
+            ...updateUserInput
+          });
+
+        const { statusCode, body } = await supertest(app)
+          .put(`/api/v1/users/${userId}`)
+          .set('Authorization', `Bearer ${jwt}`)
+          .send(updateUserInput);
+        
+        expect(statusCode).toBe(200);
+        expect(body).toEqual({
+          ...userResponse,
+          ...updateUserInput,
+        });
+        expect(findUserServiceMock).toHaveBeenCalledTimes(2);
+        expect(updateUserServiceMock).toHaveBeenCalled();
+      });
+    });
   });
 
   describe('delete user route', () => {
