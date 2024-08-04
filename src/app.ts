068e245588ecc928f4f2072e50aafe463d930026
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-08-04 17:42:22 -0400

    Format app, routes with Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
-import config from 'config';
-import createServer from './utils/server';
-import connect from './utils/connect';
-import logger from './utils/logger';
+import "dotenv/config";
+import config from "config";
+import createServer from "./utils/server";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const app = createServer();
 
-const port = config.get<number>('port');
+const port = config.get<number>("port");
 
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`);
