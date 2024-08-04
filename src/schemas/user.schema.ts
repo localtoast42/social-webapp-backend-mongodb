99406781af4c4a56c15047e8ad795002c842d24a
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-08-04 17:38:18 -0400

    Format schemas with Prettier

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,62 +1,52 @@
-import { isValidObjectId } from 'mongoose';
-import { 
-  boolean, 
-  number, 
-  object, 
-  string, 
-  enum as enum_, 
-  TypeOf 
-} from 'zod';
+import { isValidObjectId } from "mongoose";
+import { boolean, number, object, string, enum as enum_, TypeOf } from "zod";
 
 const payload = {
   body: object({
     firstName: string({
-      required_error: 'First name must be provided'
-    }).min(1, { message: 'First name must not be empty' }),
+      required_error: "First name must be provided",
+    }).min(1, { message: "First name must not be empty" }),
     lastName: string({
-      required_error: 'Last name must be provided'
-    }).min(1, { message: 'Last name must not be empty' }),
+      required_error: "Last name must be provided",
+    }).min(1, { message: "Last name must not be empty" }),
     city: string(),
     state: string(),
     country: string(),
     imageUrl: string(),
   }),
-}
+};
 
 const follow = object({
-  follow: enum_(
-    ["true", "false"], 
-    { message: "Follow must be true or false" }
-  )
+  follow: enum_(["true", "false"], { message: "Follow must be true or false" }),
 });
 
 const params = {
   params: object({
     userId: string({
-      required_error: 'userId is required',
+      required_error: "userId is required",
     }),
   }).refine((data) => isValidObjectId(data.userId), {
     message: "Invalid userId",
     path: ["userId"],
-  })
-}
+  }),
+};
 
 export const createUserSchema = object({
   body: object({
     username: string({
-      required_error: 'Username must be provided'
+      required_error: "Username must be provided",
     }),
     password: string({
-      required_error: 'Password must be provided'
+      required_error: "Password must be provided",
     }).min(6, "Password must be at least 6 characters long"),
     passwordConfirmation: string({
-      required_error: 'Password confirmation must be provided'
+      required_error: "Password confirmation must be provided",
     }),
     firstName: string({
-      required_error: 'First name must be provided'
+      required_error: "First name must be provided",
     }),
     lastName: string({
-      required_error: 'Last name must be provided'
+      required_error: "Last name must be provided",
     }),
     city: string(),
     state: string(),
@@ -90,17 +80,19 @@ export const deleteUserSchema = object({
 export const populateUsersSchema = object({
   body: object({
     userCount: number({
-      required_error: 'User count must be provided'
-    }).max(25, { message: 'No more than 25 users can be created at a time' }),
+      required_error: "User count must be provided",
+    }).max(25, { message: "No more than 25 users can be created at a time" }),
     postCount: number({
-      required_error: 'Post count must be provided'
-    }).max(10, { message: 'No more than 10 posts per user can be created at a time' }),
+      required_error: "Post count must be provided",
+    }).max(10, {
+      message: "No more than 10 posts per user can be created at a time",
+    }),
   }),
 });
 
-export type CreateUserInput = TypeOf<typeof createUserSchema>
-export type ReadUserInput = TypeOf<typeof getUserSchema>
-export type FollowUserInput = TypeOf<typeof followUserSchema>
-export type UpdateUserInput = TypeOf<typeof updateUserSchema>
-export type DeleteUserInput = TypeOf<typeof deleteUserSchema>
-export type PopulateUsersInput = TypeOf<typeof populateUsersSchema>
+export type CreateUserInput = TypeOf<typeof createUserSchema>;
+export type ReadUserInput = TypeOf<typeof getUserSchema>;
+export type FollowUserInput = TypeOf<typeof followUserSchema>;
+export type UpdateUserInput = TypeOf<typeof updateUserSchema>;
+export type DeleteUserInput = TypeOf<typeof deleteUserSchema>;
+export type PopulateUsersInput = TypeOf<typeof populateUsersSchema>;
